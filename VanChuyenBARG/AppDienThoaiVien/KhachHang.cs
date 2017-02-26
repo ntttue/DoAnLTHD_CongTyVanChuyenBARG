e08a3460deb99feed4279bd3e468e329a13d6665
ntttue <ntttue.gl@gmail.com>
2017-02-26 23:54:29 +0700

    update form GhiNhanThongTIn

---
+++ VanChuyenBARG/AppDienThoaiVien/KhachHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDienThoaiVien
+{
+    public class KhachHang
+    {
+        public int id { get; set; }
+        public string HoTen { get; set; }
+        public string SDT { get; set; }
+        public string DiaChiDon { get; set; }
+        public string GhiChu { get; set; }
+        public int LoaiXe { get; set; }
+        public DateTime ThoiDiemDat { get; set; }
+        public string TinhTrang { get; set; }
+    }
+}
---
+++ VanChuyenBARG/AppDienThoaiVien/frmGhiNhanThongTin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppDienThoaiVien
+{
+    public partial class frmGhiNhanThongTin : Form
+    {
+        public frmGhiNhanThongTin()
+        {
+            InitializeComponent();
+            callServer();
+        }
+        public void callServer()
+        {
+            try
+            {
+                using (var c = new HttpClient())
+                {
+                    c.DefaultRequestHeaders.Accept.Clear();
+                    c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    string url = "http://localhost:4771/api/KhachHangs";
+                    var response = c.PostAsJsonAsync(url, new KhachHang
+                    {
+                        HoTen = "post from client"
+                    }).Result;
+
+                    MessageBox.Show(response.ToString());
+
+
+                    //string url = "http://localhost:1898/api/simple/catList";
+
+                    ////string str =
+                    ////    c.GetStringAsync(url).Result;
+                    ////Console.WriteLine(str);
+
+                    //var response = c.GetAsync(url).Result;
+                    //if (response.StatusCode == System.Net.HttpStatusCode.OK) {
+                    //    var list = response.Content
+                    //        .ReadAsAsync<DanhMuc[]>()
+                    //        .Result;
+
+                    //    Console.WriteLine("done");
+                    //}
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+}
