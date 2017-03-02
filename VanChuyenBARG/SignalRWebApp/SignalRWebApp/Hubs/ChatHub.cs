70db0b0512f8d525d4a853e4a4310dd8ae31a9d8
ntttue <ntttue.gl@gmail.com>
2017-03-02 17:09:57 +0700

    update taixe hup

--- VanChuyenBARG/SignalRWebApp/SignalRWebApp/Hubs/ChatHub.cs
+++ VanChuyenBARG/SignalRWebApp/SignalRWebApp/Hubs/ChatHub.cs
@@ -11,14 +11,24 @@ namespace SignalRWebApp.Hubs
         public static Dictionary<string, string> dicConns = new Dictionary<string, string>();
         public void Connect(string userName)
         {
-            dicConns.Add(userName, Context.ConnectionId);
+            if (!dicConns.ContainsKey(userName))
+            {
+                dicConns.Add(userName, Context.ConnectionId);
+            }
+            else
+            {
+                dicConns[userName] = Context.ConnectionId;
+            }
+
         }
 
         public void SendPrivate(string userName, string message)
         {
-            string userConn = dicConns[userName];
-
-            Clients.Client(userConn).NotifyForSomeOne(message);
+            if (dicConns.ContainsKey(userName))
+            {
+                string userConn = dicConns[userName];
+                Clients.Client(userConn).NotifyForSomeOne(message);
+            }
         }
         ////Gọi hàm phía server
         public void Send(string userName, string message)
